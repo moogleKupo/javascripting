95c7fdeee6bd1e3c528ff43cb5b9968b7caba19e
moogleKupo <tmaine@gmx.com>
2023-06-16 18:47:15 +0000

    added condition that if array length is 0, to return an empty string (joinList function)

--- focal/joinList.js
+++ focal/joinList.js
@@ -1,5 +1,8 @@
 let joinList = function(arr) {
   let result = "";
+  if (arr.length === 0){
+    return "";
+  }
   for (let i = 0; i < arr.length - 1; i++) {
     result += String(arr[i]) + ", ";
   }
