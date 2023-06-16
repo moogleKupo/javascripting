f5d92e8f65a7061dfa064207ae0125a9a65fdb6a
moogleKupo <tmaine@gmx.com>
2023-06-16 18:41:53 +0000

    fixed joinList function

--- focal/joinList.js
+++ focal/joinList.js
@@ -1,24 +1,11 @@
-/*
- * Write a function that joins the contents of conceptList together
- * into one String, concepts, with each list item separated from
- * the previous by a comma.
- *
- * To implement this we'll create a joinList function which will take 
- * in any array of strings return a comma-separated string.
- *
- * Note: We can NOT use the built-in Array join function.
- */
-
-// Write our function (we must define it too!) below
-// ...
-let joinList = function(arr){
+let joinList = function(arr) {
   let result = "";
-  for (let i = 0; i < arr.length -1; i++) {
-result += String(arr[i]) + ", ";
+  for (let i = 0; i < arr.length - 1; i++) {
+    result += String(arr[i]) + ", ";
   }
   result += arr[arr.length - 1];
   return result;
-} ;
+};
 // Test / Driver Code below...
 const conceptList = ["gists", "types", "operators", "iteration", "problem solving"];
 const concepts = joinList(conceptList);
