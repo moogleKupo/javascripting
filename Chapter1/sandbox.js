7de760b462c18e329e56b3ec85d342de6aacd018
moogleKupo <tmaine@gmx.com>
2023-06-21 21:52:10 +0000

    starting countOnly

--- Chapter1/sandbox.js
+++ Chapter1/sandbox.js
@@ -1,10 +1,8 @@
-const password = 'pass';
-if(password.length >= 12){
-  console.log("password is a good length!");
-} else if (password.length <= 12 && password.length >= 8){
-  console.log('Nice one!');
-} else if (password.length > 0 && password.length < 8){
-    console.log('Not long enough!');
-  } else {
-    console.log('Please enter a password!')
-  }
+// const greet = function(){
+//   return 'hello world';
+// }
+
+const greet = () => 'hello world';
+
+const result = greet();
+console.log(result);
---
+++ javascripting/variables.js
@@ -0,0 +1,3 @@
+const example = 'some string';
+console.log(example);
+javascripting verify variables.javascripting
