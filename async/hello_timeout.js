c3690a59974fea52746a0ea20790dcbe149ecd85
moogleKupo <tmaine@gmx.com>
2023-07-11 21:08:09 +0000

    typewriter app complete

---
+++ async/hello_timeout.js
@@ -0,0 +1,12 @@
+const delayedGreeting1 = function() {
+  console.log('4311o ');
+}
+const delayedGreeting2 = function() {
+  console.log('th3r3');
+}
+const delayedGreeting3 = function() {
+  console.log('w0r1d');
+}
+setTimeout(delayedGreeting1, 1000);
+setTimeout(delayedGreeting2, 3000);
+setTimeout(delayedGreeting3, 6000);
---
+++ async/typewriter/typewriter.js
@@ -0,0 +1,12 @@
+const sentence = "hello there from lighthouse labs";
+let delay = 0;
+for (let i = 0; i <sentence.length; i++){
+  setTimeout(() => {
+    process.stdout.write(sentence[i])
+  }, delay);
+delay += 50;
+}
+setTimeout(() => {
+  console.log();
+}, delay);
+
